352f97525d11ab7f4d7d34a87aac3e8ab5349fe5
Hasan Hasanzade <101423091+hasan-hasanzade@users.noreply.github.com>
2022-10-14 23:12:21 +0400

    map added

--- main.js
+++ main.js
@@ -242,3 +242,13 @@ var swiper = new Swiper(".iphk__slider", {
     }
   },
 });
+
+ymaps.ready(init);
+        function init(){
+            var myMap = new ymaps.Map("map", {
+                center: [55.76, 37.64],
+                zoom: 12,
+                controls: ['zoomControl']
+            });
+            
+        }
